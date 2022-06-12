6b844eac08bec6ff168c6756e60d759da73eb4e0
EllyIMA <wiz33@yandex.ru>
2022-06-12 20:00:24 +0300

    Доработка

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import telebot
 from _token import token
-from remains import AV_key_bot
+from telebot import types
 
 bot = telebot.TeleBot(token=token)
+
+@bot.message_handler(func=lambda message: True)
+def buttons(message):
+	markup_inline = types.InlineKeyboardMarkup()
+	item_yes = types.InlineKeyboardButton(text= 'ДА', callback_data='y')
+	item_no = types.InlineKeyboardButton(text='НЕТ', callback_data='n')
+
+	markup_inline.add(item_yes, item_no)
+	bot.send_message(message.chat.id, 'Желаете узнать небольшую информацию о вас',
+		reply_markup=markup_inline
+	)
+
+bot.callback_query_handler(func = lambda call: True)
+def answer(call):
+	if call.data =='yes':
+		pass
+	if call.data == 'no':
+		pass
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
