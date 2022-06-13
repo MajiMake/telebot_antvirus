719c4f95b7261800c75f1865ab5582b98dbe4630
EllyIMA <wiz33@yandex.ru>
2022-06-13 17:00:36 +0300

    Кнопки работают

--- main.py
+++ main.py
@@ -3,31 +3,43 @@ from _token import token
 from telebot import types
 
 bot = telebot.TeleBot(token=token)
+list = {'1': 'a', '2': 'б', '3': 'в', '4': 'г', '5': 'д', '6': 'е', '7': 'ж', '8': 'з', '9': 'й', '10': 'к',
+        }
 
-@bot.message_handler(func=lambda message: True)
-def buttons(message):
-	markup_inline = types.InlineKeyboardMarkup()
-	item_yes = types.InlineKeyboardButton(text= 'ДА', callback_data='y')
-	item_no = types.InlineKeyboardButton(text='НЕТ', callback_data='n')
 
-	markup_inline.add(item_yes, item_no)
-	bot.send_message(message.chat.id, 'Желаете узнать небольшую информацию о вас',
-		reply_markup=markup_inline
-	)
+@bot.message_handler(commands=['start'])
+def start_func(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    count = 1
+    button1 = None
+    for callback, text in list.items():
+        if count == 1:
+            button1 = button_creator(callback, text)
+            count = 2
+        else:
+            button2 = button_creator(callback, text)
+            markup_inline.add(button1,button2)
 
-bot.callback_query_handler(func = lambda call: True)
+    bot.send_message(message.chat.id, 'Выберите ключ', reply_markup=markup_inline)
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
 def answer(call):
-	if call.data =='yes':
-		pass
-	if call.data == 'no':
-		pass
+    if call.data == 'y':
+        markup_inline = types.InlineKeyboardMarkup()
+        one_pc = button_creator('1pc', '1 ПК')
+        three_pc = button_creator('3pc', '3 ПК')
+        markup_inline.add(one_pc, three_pc)
+        bot.send_message(call.from_user.id, 'на сколько пк',
+                         reply_markup=markup_inline
+                         )
+        bot.send_message(call.message.chat.id, type(call.message.chat.id))
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Test, how are you doing?")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+def button_creator(callback, text):
+    button = types.InlineKeyboardButton(text=text, callback_data=callback)
+    return button
+
 
 bot.infinity_polling()
